9f56c0d9baf40e43a8be24dc1aa6cb0c60b7e3b9
Sunil Mohan Adapa <sunil@medhas.org>
2018-05-24 14:44:36 +0530

    internal: Finish implementation for Dreamplug
    Signed-off-by: Sunil Mohan Adapa <sunil@medhas.org>
    Reviewed-by: James Valleroy <jvalleroy@mailbox.org>

--- freedommaker/builder.py
+++ freedommaker/builder.py
@@ -586,10 +586,53 @@ class DreamPlugImageBuilder(ARMImageBuilder):
     machine = 'dreamplug'
     kernel_flavor = 'marvell'
     boot_filesystem_type = 'vfat'
+    flash_kernel_name = 'Globalscale Technologies Dreamplug'
 
-    def install_boot_loader(self, state):
+    @staticmethod
+    def install_boot_loader(state):
         """Install the boot loader onto the image."""
-        raise NotImplementedError
+        # flash-kernel's hook-functions provided to mkinitramfs have the
+        # unfortunate side-effect of creating /conf/param.conf in the initrd
+        # when run from our emulated chroot environment, which means our root=
+        # on the kernel command line is completely ignored!  repack the initrd
+        # to remove this evil...
+        script = r'''
+set -e
+set -x
+set -o pipefail
+
+kernelVersion=$(ls /usr/lib/*/kirkwood-dreamplug.dtb | head -1 | cut -d/ -f4)
+version=$(echo $kernelVersion | sed 's/linux-image-\(.*\)/\1/')
+initRd=initrd.img-$version
+vmlinuz=vmlinuz-$version
+
+mkdir /tmp/initrd-repack
+
+(cd /tmp/initrd-repack ; \
+    zcat /boot/$initRd | cpio -i ; \
+    rm -f conf/param.conf ; \
+    find . | cpio --quiet -o -H newc | \
+    gzip -9 > /boot/$initRd )
+
+rm -rf /tmp/initrd-repack
+
+(cd /boot ; \
+    cp /usr/lib/$kernelVersion/kirkwood-dreamplug.dtb dtb ; \
+    cat $vmlinuz dtb >> temp-kernel ; \
+    mkimage -A arm -O linux -T kernel -n "Debian kernel ${version}" \
+    -C none -a 0x8000 -e 0x8000 -d temp-kernel uImage ; \
+    rm -f temp-kernel ; \
+    mkimage -A arm -O linux -T ramdisk -C gzip -a 0x0 -e 0x0 \
+    -n "Debian ramdisk ${version}" \
+    -d $initRd uInitrd )
+'''
+        library.run_in_chroot(state, ['bash', '-c', script])
+
+        logger.info('Adding a getty on the serial port')
+        script = r'''
+echo "T0:12345:respawn:/sbin/getty -L ttyS0 115200 vt100" >> /etc/inittab
+'''
+        library.run_in_chroot(state, ['bash', '-c', script])
 
 
 class RaspberryPiImageBuilder(ARMImageBuilder):
